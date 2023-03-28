1d4aba1314135ca3557200321f9687c5f1ae0ec3
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-03-28 15:02:18 +0300

    feat, style: add handel.py for admin and user folder/ add comments and simples

--- app/handlers/main_handler.py
+++ app/handlers/main_handler.py
@@ -1,8 +1,14 @@
 from aiogram import Dispatcher
+from app.handlers.admin.handler import register_admin_handlers
+from app.handlers.users.handler import register_user_handlers
+from app.handlers.other.handler import register_other_handlers
 
 
-# def register_all_handlers(dp: Dispatcher):
-    # handlers = [register_command_handlers, register_other_handlers, register_callback_query_handlers]
+def register_all_handlers(dp: Dispatcher) -> None:
+    """
+    Регистрирует все обработчики
+    """
+    handlers = [register_admin_handlers, register_user_handlers, register_other_handlers]
 
-    # for handler in handlers:
-    #     handler(dp)
+    for handler in handlers:
+        handler(dp)
