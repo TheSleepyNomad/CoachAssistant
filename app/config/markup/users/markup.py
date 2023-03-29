68b4e22ffa5856730b19e0c4e537b85fb4664e35
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-03-29 15:23:52 +0300

    feat: add start msg and admin role checking

--- app/config/markup/users/markup.py
+++ app/config/markup/users/markup.py
@@ -4,7 +4,7 @@ Generate markup for casual users
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 
-def create_admin_start_markup() -> InlineKeyboardMarkup:
+def create_user_start_markup() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup().add(InlineKeyboardButton('Записаться на тренировку', callback_data=' '))\
         .add(InlineKeyboardButton(text='Процесс тренировки', callback_data=' '))
     
