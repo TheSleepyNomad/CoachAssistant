e72cbae5b153d540c3fc36a47f543cf0dae94ea9
Sobchenko Roman <sobchenko_roman@mail.ru>
2023-04-09 21:41:10 +0300

    feat: write new start markup for user

--- app/config/markup/users/markup.py
+++ app/config/markup/users/markup.py
@@ -5,7 +5,18 @@ from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, C
 
 
 def create_user_start_markup() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup().add(InlineKeyboardButton('Записаться на тренировку', callback_data=' '))\
-        .add(InlineKeyboardButton(text='Процесс тренировки', callback_data=' '))
-    
+    markup = InlineKeyboardMarkup()\
+        .add(InlineKeyboardButton('Записаться', callback_data='record'))\
+        .add(InlineKeyboardButton('О школе', callback_data='about_school'))\
+        .add(InlineKeyboardButton('Расписание тренировок', callback_data='gym_calendar'))\
+        
+    return markup
+
+def description_school_markup() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup()\
+        .add(InlineKeyboardButton('Описание стиля', callback = ' '))\
+        .add(InlineKeyboardButton('Лучшие ученики', callback = ' '))\
+        .add(InlineKeyboardButton('Тренеры школы', callback = ' '))\
+        .add(InlineKeyboardButton('Назад', callback = ' '))\
+        
     return markup
