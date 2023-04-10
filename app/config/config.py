dd8f56ae795de19543c530607dc004224ddd0011
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-04-10 12:06:03 +0300

    feat: add sqlachemy db engine and singleton

--- app/config/config.py
+++ app/config/config.py
@@ -4,3 +4,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 # папка с логами
 LOG_DIR = BASE_DIR.joinpath('logs').joinpath('log.json')
+# путь к базе данных
+DB_PATH = BASE_DIR.joinpath('db').joinpath('data.sqlite')
