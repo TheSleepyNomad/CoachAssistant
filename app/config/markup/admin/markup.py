fb45599eb71ea95f51d403ad7f909540af597937
Ilya TheSleepyNomad Pankov <iluxaan@mail.ru>
2023-04-11 14:13:41 +0300

    refactor: new admin start markup

--- app/config/markup/admin/markup.py
+++ app/config/markup/admin/markup.py
@@ -2,11 +2,17 @@
 Generate markup for user with admin role(owner)
 """
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from app.config.markup.buttons import Btn
 
 
 def create_admin_start_markup() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup().add(InlineKeyboardButton('Мой журнал', callback_data=' '))\
-        .add(InlineKeyboardButton(text='Настройки', callback_data=' '))
+    """
+    Создает раскладку кнопок для стартового меню администратора
+    """
+    markup = InlineKeyboardMarkup()\
+        .add(InlineKeyboardButton(Btn.journal, callback_data='journal'))\
+        .add(InlineKeyboardButton(text=Btn.reports, callback_data='reports'))
+    
     return markup
 
 def create_admin_journal_markup() -> InlineKeyboardMarkup:
--- app/config/markup/buttons.py
+++ app/config/markup/buttons.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 @dataclass(slots=True, frozen=True)
 class Btn:
     journal = '\U0001F3EA' + ' Журнал'
-    catalog = '\U0001F3F7' + ' Отчеты'
+    reports = '\U0001F3F7' + ' Отчеты'
     pay_card = '\U0001F4B3' + ' Оплатить'
